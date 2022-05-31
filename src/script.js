c35602519e1eac74277e7c0b60c8783cc47f783a
WilhelmWalters <102514633+WilhelmWalters@users.noreply.github.com>
2022-05-31 23:47:56 +0100

    Set up api call

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
-let inputBar = document.querySelector("#input-bar");
-inputBar.addEventListener("submit", rollDice);
-
-function rollDice() {
-  let randomNumber = Math.floor(Math.random() * 6 + 1);
-  alert(`${randomNumber}`);
+function presentData(response) {
+  console.log(response.data);
 }
+
+let apiKey = "489055a913f2ed412a9598a039efc966";
+let cityName = "New York";
+let units = `metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(presentData);
