36fe56064362b7754be5ad304c8a85e77e3cd30b
WilhelmWalters <102514633+WilhelmWalters@users.noreply.github.com>
2022-06-02 11:10:08 +0100

    Connected API

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
 function presentData(response) {
   console.log(response.data);
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let name = document.querySelector("#city-name");
+  name.innerHTML = response.data.name;
 }
 
 let apiKey = "489055a913f2ed412a9598a039efc966";
-let cityName = "New York";
+let searchedName = `New York`;
 let units = `metric`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedName}&appid=${apiKey}&units=${units}`;
 
 axios.get(apiUrl).then(presentData);
